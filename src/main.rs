7c768b5cef5ebdaed455c1b99d668b619cbeb54c
johnsonyuen <j@johnsonyuen.com>
2021-02-13 10:49:41 +1100

    pg35

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use zero2prod::run;
+use std::net::TcpListener;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:9090")
+        .expect("Failed to acquire port 9090");
+    run(listener)?.await
 }
