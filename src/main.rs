a316f0f24ebd84fae0cdbfae2eb9d10435461acc
johnsonyuen <j@johnsonyuen.com>
2021-02-13 10:33:14 +1100

    Pg32. Cargo.toml from internet

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:9090")?
-        .run()
-        .await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
 }
